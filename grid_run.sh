f0416db698163577dbfabf3111df91b66420a39e
polyphant1 <chrisl1991@hotmail.co.uk>
2021-03-25 15:56:48 +0000

    update modules for cosma7

--- grid_run.sh
+++ grid_run.sh
@@ -1,15 +1,20 @@
-#!/bin/tcsh
-#BSUB -L /bin/tcsh
-#BSUB -q cosma
-#BSUB -P durham
-#BSUB -J density_gridder
-#BSUB -W 00:35
-#BSUB -n 24
-#BSUB -o logs/grid_out.%J
-#BSUB -e logs/grid_err.%J
+#!/bin/bash
+#SBATCH -p cosma7
+#SBATCH -A dp004
+#SBATCH --job-name=density_gridder_ref100
+#SBATCH --output=logs/grid_out_ref.%J
+#SBATCH --error=logs/grid_err_ref.%J
+#SBATCH -t 00:35
+#SBATCH --ntasks 24
+# #SBATCH --exclusive
 
 module purge
-module load intel_comp/c4/2013.1.046 gsl/1.16 platform_mpi/9.1.2 hdf5/1.8.12
+module load intel_comp/2021.1.0
+module load gsl/2.4
+module load intel_mpi/2020-update2
+module load hdf5/1.10.3 
+
+mpicc -I/usr/include/hdf5/serial -o gridder.x -L/usr/lib/x86_64-linux-gnu/hdf5/serial gridder.c -lhdf5 -lm -lgmp
 
 mpirun gridder.x
 
--- makefile
+++ makefile
@@ -1,17 +1,10 @@
 SHELL:=/bin/tcsh
 
-## Alternative compilation options:
-# module load intel_comp/c4/2013.1.046 gsl/1.16 platform_mpi/9.1.2 hdf5/1.8.12; 
-# mpicc -I/usr/include/hdf5/serial -o gridder.x -L/usr/lib/x86_64-linux-gnu/hdf5/serial gridder.c -lhdf5 -lm -lgmp
-
 all:
 	. /usr/share/Modules/init/tcsh; \
     module purge; \
-	module unload intel_comp/c4/2015; \
-	module unload gnu_comp; \
-	module unload platform_mpi; \
-	module load intel_comp/c5/2013.0.028; \
-	module load hdf5/1.8.9; \
+	module load intel_comp; \
+	module load gsl; \
 	module load openmpi; \
 	module load hdf5; \
 	mpicc -o gridder.x gridder.c -lhdf5 -lm -lgmp
